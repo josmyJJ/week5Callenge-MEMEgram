7faec031771284961c540a31ece6f02bf68398d0
josmyJJ <josemyjoseph@gmail.com>
2018-06-28 16:41:31 -0400

    initial commit

---
+++ src/main/java/com/example/lesson12/ActorRepository.java
@@ -0,0 +1,6 @@
+package com.example.lesson12;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long>{
+}
---
+++ src/main/java/com/example/lesson12/Lesson12Application.java
@@ -0,0 +1,12 @@
+package com.example.lesson12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson12Application.class, args);
+	}
+}
